3c67a0b5c62c9791dc332753e161674a5d662837
Mae Bridgeman <matthew@thebridgemans.net>
2025-02-26 15:25:45 -0800

    Initial Commit

---
+++ 310MapKitDemo/310MapKitDemo/_10MapKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  _10MapKitDemoApp.swift
+//  310MapKitDemo
+//
+//  Created by Mae Bridgeman on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct _10MapKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- README.md
+++
@@ -1,5 +0,0 @@
-# 310-Demonstration
-
-This app is for the presentation demonstration, NOT the full AR app.
-
-Created by Mae, Ady, Daria, Alin and Payton
